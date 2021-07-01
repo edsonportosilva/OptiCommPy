c2fe8dc0eb375a4eb4fcbbe310c4bf1d82ed87eb
Edson Porto da Silva <edsonporto88@gmail.com>
2021-07-01 18:49:49 -0300

    Update models.py

--- jupyter notebooks/utils/models.py
+++ jupyter notebooks/utils/models.py
@@ -7,9 +7,9 @@ def mzm(Ai, Vπ, u, Vb):
     :param Vπ: Vπ-voltage
     :param Vb: bias voltage
     :param u:  input driving signal
-    :param Ai: input amplitude of the optical signal 
+    :param Ai: input optical signal 
     
-    :return Ao: output amplitude of the optical signal
+    :return Ao: output optical signal
     """
     π  = np.pi
     Ao = Ai*np.cos(0.5/Vπ*(u+Vb)*π)
