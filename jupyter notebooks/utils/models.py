16b9a0a3e6ccdef780772ebb7e64b9f2a1da0b34
Edson Porto da Silva <edsonporto88@gmail.com>
2021-09-19 01:00:57 -0300

    Update models.py

--- jupyter notebooks/utils/models.py
+++ jupyter notebooks/utils/models.py
@@ -6,12 +6,28 @@ def mzm(Ai, Vπ, u, Vb):
     
     :param Vπ: Vπ-voltage
     :param Vb: bias voltage
-    :param u:  input driving signal
-    :param Ai: input optical signal 
+    :param u:  modulator's driving signal (real-valued)
+    :param Ai: amplitude of the input CW carrier
     
     :return Ao: output optical signal
     """
     π  = np.pi
     Ao = Ai*np.cos(0.5/Vπ*(u+Vb)*π)
+    
+    return Ao
+
+def iqm(Ai, u, Vπ, VbI, VbQ):
+    """
+    IQ modulator 
+    
+    :param Vπ: MZM Vπ-voltage
+    :param VbI: in-phase MZM bias voltage
+    :param VbQ: quadrature MZM bias voltage    
+    :param u:  modulator's driving signal (complex-valued baseband)
+    :param Ai: amplitude of the input CW carrier
+    
+    :return Ao: output optical signal
+    """
+    Ao = mzm(Ai/np.sqrt(2), Vπ, u.real, VbI) + 1j*mzm(Ai/np.sqrt(2), Vπ, u.imag, VbQ)
     
     return Ao
