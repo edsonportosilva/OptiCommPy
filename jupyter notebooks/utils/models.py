cb170d4cdd704b73c98c49b8d45752292b899013
Edson Porto da Silva <edsonporto88@gmail.com>
2021-10-05 00:20:53 -0300

    Update models.py

--- jupyter notebooks/utils/models.py
+++ jupyter notebooks/utils/models.py
@@ -1,5 +1,6 @@
 import numpy as np
 from numpy.fft import fft, ifft, fftfreq
+import scipy.constants as const
 
 def mzm(Ai, Vπ, u, Vb):
     """
@@ -46,8 +47,8 @@ def linFiberCh(Ei, L, alpha, D, Fc, Fs):
     
     :return Eo: optical signal at the output of the fiber
     """
-    c  = 299792458   # speed of light [m/s](vacuum)    
-    c_kms = c/1e3
+    #c  = 299792458   # speed of light [m/s](vacuum)    
+    c_kms = const.c/1e3
     λ  = c_kms/Fc
     α  = alpha/(10*np.log10(np.exp(1)))
     β2 = -(D*λ**2)/(2*np.pi*c_kms)
@@ -134,3 +135,79 @@ def coherentReceiver(Es, Elo, Rd=1):
     sQ = balancedPD(Eo[2,:], Eo[3,:], Rd)
     
     return sI + 1j*sQ
+
+def edfa(Ei, G, nf, Fc, Fs):
+    '''
+    Simple EDFA model
+    
+    '''
+    nf_lin   = 10**(nf/10)
+    G_lin    = 10**(G/10)
+    nsp      = (G_lin*nf_lin - 1)/(2*(G_lin - 1))
+    N_ase    = (G_lin - 1)*nsp*const.h*Fc
+    p_noise  = N_ase*Fs    
+    noise    = np.random.normal(0, np.sqrt(p_noise), Ei.shape) + 1j*np.random.normal(0, np.sqrt(p_noise), Ei.shape)
+    return Ei*np.sqrt(G_lin) + noise
+
+def ssfm(Ei, Fs, Ltotal, Lspan, hz=0.5, alpha=0.2, gamma=1.3, D=16, Fc=193.1e12, amp='edfa', NF=4.5):      
+    '''
+    Split-step Fourier method (symmetric, single-pol.)
+
+    :param Ei: input signal
+    :param Ltotal: total fiber length [km]
+    :param Lspan: span length [km]
+    :param hz: step-size for the split-step Fourier method [km][default: 0.5 km]
+    :param alpha: fiber attenuation parameter [dB/km][default: 0.2 dB/km]
+    :param D: chromatic dispersion parameter [ps/nm/km][default: 16 ps/nm/km]
+    :param gamma: fiber nonlinear parameter [1/W/km][default: 1.3 1/W/km]
+    :param amp: 'edfa', 'ideal', or 'None. [default:'edfa']
+    :param NF: edfa noise figure [dB] [default: 4.5 dB]
+    :param Fc: carrier frequency [Hz] [default: 193.1e12 Hz]
+    :param Fs: sampling frequency [Hz]
+
+    :return Ech: propagated signal
+    '''             
+    #c = 299792458   # speed of light (vacuum)
+    c_kms = const.c/1e3
+    λ  = c_kms/Fc
+    α  = alpha/(10*np.log10(np.exp(1)))
+    β2 = -(D*λ**2)/(2*np.pi*c_kms)
+    γ  = gamma
+            
+    Nfft = len(Ein)
+
+    ω = 2*np.pi*Fs*fftfreq(Nfft)
+    
+    Nspans = int(np.floor(Ltotal/Lspan))
+    Nsteps = int(np.floor(Lspan/hz))
+    
+    Ech = Ei.reshape(len(Ei),)  
+      
+    linOperator = np.exp(-(α/2)*(hz/2) + 1j*(β2/2)*(ω**2)*(hz/2))
+    
+    for spanN in tqdm(range(1, Nspans+1)):   
+        Ech = fft(Ech) #single-polarization field
+        
+        # fiber propagation step
+        for stepN in range(1, Nsteps+1):            
+            # First linear step (frequency domain)
+            Ech = Ech*linOperator            
+
+            # Nonlinear step (time domain)
+            Ech = ifft(Ech)
+            Ech = Ech*np.exp(1j*γ*(Ech*np.conj(Ech))*hz)
+
+            # Second linear step (frequency domain)
+            Ech = fft(Ech)       
+            Ech = Ech*linOperator           
+
+        # amplification step
+        Ech = ifft(Ech)
+        if amp =='edfa':
+            Ech = edfa(Ech, alpha*Lspan, NF, Fc, Fs)
+        elif amp =='ideal':
+            Ech = Ech*np.exp(α/2*Nsteps*hz)
+        elif amp == None:
+            Ech = Ech*np.exp(0);         
+          
+    return Ech.reshape(len(Ech), 1)
