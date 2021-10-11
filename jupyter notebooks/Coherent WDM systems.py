473e79d3e9203ae869b7913453cb8e3eaf7f9e6b
Edson Porto da Silva <edsonporto88@gmail.com>
2021-10-11 16:01:45 -0300

    update notebooks

--- jupyter notebooks/Coherent WDM systems.py
+++ jupyter notebooks/Coherent WDM systems.py
@@ -21,7 +21,7 @@ from numpy.random import normal
 from commpy.utilities  import signal_power, upsample
 from commpy.modulation import QAMModem
 from utils.dsp import firFilter, pulseShape, lowPassFIR, edc, fourthPowerFOE, dbp, cpr
-from utils.models import mzm, linFiberCh, iqm, ssfm, edfa, phaseNoise, coherentReceiver
+from utils.models import mzm, linFiberCh, iqm, ssfm, edfa, phaseNoise, coherentReceiver, manakov_ssf
 from utils.tx import simpleWDMTx
 from utils.core import parameters
 
@@ -328,5 +328,235 @@ def powerProfile(Pin, alpha, Lspan, Nspans):
 
 
 powerProfile(10, 0.2, 80, 10)
+
+# ## Polarization multiplexed WDM signal
+
+# **WDM signal generation**
+
+# +
+# Parâmetros do transmissor:
+param = parameters()
+param.M   = 16           # ordem do formato de modulação
+param.Rs  = 32e9         # taxa de sinalização [baud]
+param.SpS = 16           # número de amostras por símbolo
+param.Nbits = 60000      # número de bits
+param.pulse = 'rrc'      # formato de pulso
+param.Ntaps = 4096       # número de coeficientes do filtro RRC
+param.alphaRRC = 0.01    # rolloff do filtro RRC
+param.Pch_dBm = 1        # potência média por canal WDM [dBm]
+param.Nch     = 9        # número de canais WDM
+param.Fc      = 193.1e12 # frequência central do espectro WDM
+param.freqSpac = 40e9    # espaçamento em frequência da grade de canais WDM
+param.Nmodes = 2         # número de modos de polarização
+
+sigWDM_Tx, symbTx_, param = simpleWDMTx(param)
+
+freqGrid = param.freqGrid
+# -
+# **Nonlinear fiber propagation with the split-step Fourier method**
+
+# +
+linearChannel = False
+
+# optical channel parameters
+paramCh = parameters()
+paramCh.Ltotal = 800   # km
+paramCh.Lspan  = 80    # km
+paramCh.alpha = 0.2    # dB/km
+paramCh.D = 16         # ps/nm/km
+paramCh.Fc = 193.1e12  # Hz
+paramCh.hz = 0.5       # km
+paramCh.gamma = 1.3    # 1/(W.km)
+
+if linearChannel:
+    paramCh.hz = paramCh.Lspan  # km
+    paramCh.gamma = 0   # 1/(W.km)
+
+Fs = param.Rs*param.SpS
+sigWDM, paramCh = manakov_ssf(sigWDM_Tx, Fs, paramCh) 
+
+
+receivedSignal = sigWDM.copy()
+transmSymbols  = symbTx_.copy()
+# -
+
+# **Optical WDM spectrum before and after transmission**
+
+# plot psd
+plt.figure()
+plt.xlim(paramCh.Fc-Fs/2,paramCh.Fc+Fs/2);
+plt.psd(sigWDM_Tx[:,0], Fs=param.SpS*param.Rs, Fc=paramCh.Fc, NFFT = 4*1024, sides='twosided', label = 'WDM spectrum - Tx')
+plt.psd(sigWDM[:,0], Fs=Fs, Fc=paramCh.Fc, NFFT = 4*1024, sides='twosided', label = 'WDM spectrum - Rx')
+plt.legend(loc='lower left')
+plt.title('optical WDM spectrum');
+
+
+# **WDM channels coherent detection and demodulation**
+
+# +
+### Receiver
+
+# parameters
+chIndex  = 4    # index of the channel to be demodulated
+polIndex = 0
+plotPSD  = True
+
+Fa = param.SpS*param.Rs
+Fc = paramCh.Fc
+Ta = 1/Fa
+mod = QAMModem(m=param.M)
+
+print('Demodulating channel #%d , fc: %.4f THz, λ: %.4f nm\n'\
+      %(chIndex, (Fc + freqGrid[chIndex])/1e12, const.c/(Fc + freqGrid[chIndex])/1e-9))
+
+sigWDM = receivedSignal[:,polIndex].reshape(len(sigWDM),)
+symbTx = transmSymbols[:,polIndex,chIndex].reshape(len(symbTx_),)
+
+# local oscillator (LO) parameters:
+FO      = 64e6                 # frequency offset
+Δf_lo   = freqGrid[chIndex]+FO  # downshift of the channel to be demodulated
+lw      = 0*100e3                 # linewidth
+Plo_dBm = 10                    # power in dBm
+Plo     = 10**(Plo_dBm/10)*1e-3 # power in W
+ϕ_lo    = 0                     # initial phase in rad    
+
+print('Local oscillator P: %.2f dBm, lw: %.2f kHz, FO: %.2f MHz\n'\
+      %(Plo_dBm, lw/1e3, FO/1e6))
+
+# generate LO field
+π       = np.pi
+t       = np.arange(0, len(sigWDM))*Ta
+ϕ_pn_lo = phaseNoise(lw, len(sigWDM), Ta)
+sigLO   = np.sqrt(Plo)*np.exp(1j*(2*π*Δf_lo*t + ϕ_lo + ϕ_pn_lo))
+
+# single-polarization coherent optical receiver
+sigRx = coherentReceiver(sigWDM, sigLO)
+
+# Rx filtering
+
+# Matched filter
+if param.pulse == 'nrz':
+    pulse = pulseShape('nrz', param.SpS)
+elif param.pulse == 'rrc':
+    pulse = pulseShape('rrc', param.SpS, N=param.Ntaps, alpha=param.alphaRRC, Ts=1/param.Rs)
+
+pulse = pulse/np.max(np.abs(pulse))            
+sigRx = firFilter(pulse, sigRx)
+
+# plot psd
+if plotPSD:
+    plt.figure();
+   # plt.ylim(-250,-50);
+    plt.psd(sigRx, Fs=Fa, NFFT = 16*1024, sides='twosided', label = 'Spectrum of the received signal')
+    plt.legend(loc='upper left');
+    plt.xlim(-Fa/2,Fa/2);
+
+fig, (ax1, ax2, ax3, ax4) = plt.subplots(1, 4,figsize=(15,4.5))
+fig.suptitle('Sequence of DSP blocks')
+
+ax1.plot(sigRx.real, sigRx.imag,'.', markersize=4)
+ax1.axis('square')
+ax1.title.set_text('Output of coherent front-end')
+ax1.grid()
+
+# digital backpropagation
+# hzDBP = 5
+# Pin   = 10**(param.Pch_dBm/10)*1e-3
+# sigRx = sigRx/np.sqrt(signal_power(sigRx))
+# sigRx = dbp(np.sqrt(Pin)*sigRx, Fa, paramCh.Ltotal, paramCh.Lspan, hzDBP, paramCh.alpha, paramCh.gamma, paramCh.D, paramCh.Fc)
+# sigRx = sigRx.reshape(len(sigRx),)
+# sigRx = firFilter(pulse, sigRx)
+    
+# CD compensation
+sigRx = edc(sigRx, paramCh.Ltotal, paramCh.D, Fc-Δf_lo, Fa)
+
+# simple timing recovery
+varVector = np.var((sigRx.T).reshape(-1,param.SpS), axis=0) # finds best sampling instant
+sampDelay = np.where(varVector == np.amax(varVector))[0][0]
+
+# downsampling
+sigRx = sigRx[sampDelay::param.SpS]
+
+discard = 1000
+ind = np.arange(discard, sigRx.size-discard)
+
+# symbol normalization
+sigRx = sigRx/np.sqrt(signal_power(sigRx[ind]))
+
+# plot constellation after CD compensation
+ax2.plot(sigRx.real, sigRx.imag,'.', markersize=4)
+ax2.axis('square')
+ax2.title.set_text('After CD comp.')
+ax2.grid()
+
+# calculate time delay due to walkoff
+symbDelay = np.argmax(signal.correlate(np.abs(symbTx), np.abs(sigRx)))-sigRx.size+1 
+
+# compensate walkoff time delay
+sigRx = np.roll(sigRx, symbDelay)
+
+# symbol normalization
+sigRx = sigRx/np.sqrt(signal_power(sigRx[ind]))
+
+# estimate and compensate LO frequency offset
+fo = fourthPowerFOE(sigRx, 1/param.Rs)
+print('Estimated FO : %3.4f MHz'%(fo/1e6))
+
+sigRx = sigRx*np.exp(-1j*2*π*fo*np.arange(0,len(sigRx))/param.Rs)
+
+# plot constellation after LO frequency offset compensation
+ax3.plot(sigRx[ind].real, sigRx[ind].imag,'.', markersize=4)
+ax3.axis('square')
+ax3.title.set_text('After CFR (4th-power FOE)')
+ax3.grid()
+
+# compensate phase noise (carrier phase recovery - cpr)
+windowSize = 40
+c  = mod.constellation/np.sqrt(mod.Es)
+sigRx, ϕ, θ = cpr(sigRx, windowSize, c, symbTx)
+
+# plot phases estimated by cpr
+phaseOffSet = np.mean(np.roll(ϕ_pn_lo[::param.SpS], symbDelay)-θ)
+plt.figure()
+plt.plot(np.roll(ϕ_pn_lo[::param.SpS], symbDelay), label='phase of the LO');
+plt.plot(θ+phaseOffSet, label='phase estimated by CPR');
+plt.grid()
+plt.xlim(0,θ.size)
+plt.legend();
+
+# correct (possible) phase ambiguity
+rot = np.mean(symbTx[ind]/sigRx[ind])
+sigRx  = rot*sigRx
+
+# symbol normalization
+sigRx = sigRx/np.sqrt(signal_power(sigRx[ind]))
+
+# plot constellation after cpr
+ax4.plot(sigRx[ind].real, sigRx[ind].imag,'.', markersize=4)
+ax4.axis('square')
+ax4.title.set_text('After CPR (DD-PLL)')
+ax4.grid()
+
+# estimate SNR of the received constellation
+SNR = signal_power(symbTx[ind])/signal_power(sigRx[ind]-symbTx[ind])
+
+# hard decision demodulation of the received symbols    
+bitsRx = mod.demodulate(np.sqrt(mod.Es)*sigRx, demod_type = 'hard') 
+bitsTx = mod.demodulate(np.sqrt(mod.Es)*symbTx, demod_type = 'hard') 
+
+err = np.logical_xor(bitsRx[discard:bitsRx.size-discard], 
+                     bitsTx[discard:bitsTx.size-discard])
+BER = np.mean(err)
+
+print('Estimated SNR = %.2f dB \n'%(10*np.log10(SNR)))
+print('Total counted bits = %d  '%(err.size))
+print('Total of counted errors = %d  '%(err.sum()))
+print('BER = %.2e  '%(BER))
+
+plt.figure()
+plt.plot(err,'o', label = 'errors location')
+plt.legend()
+plt.grid()
+# -
 
 
