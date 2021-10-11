643647a14a9c429e985922103856a4d539c379bb
Edson Porto da Silva <edsonporto88@gmail.com>
2021-10-11 16:01:35 -0300

    add manakov model

--- jupyter notebooks/utils/models.py
+++ jupyter notebooks/utils/models.py
@@ -248,6 +248,109 @@ def ssfm(Ei, Fs, paramCh):
           
     return Ech.reshape(len(Ech),), paramCh
 
+def manakov_ssf(Ei, Fs, paramCh):      
+    """
+    Manakov model split-step Fourier (symmetric, dual-pol.)
+
+    :param Ei: input signal
+    :param Fs: sampling frequency of Ei [Hz]
+    :param paramCh: object with physical parameters of the optical channel
+    
+    :paramCh.Ltotal: total fiber length [km][default: 400 km]
+    :paramCh.Lspan: span length [km][default: 80 km]
+    :paramCh.hz: step-size for the split-step Fourier method [km][default: 0.5 km]
+    :paramCh.alpha: fiber attenuation parameter [dB/km][default: 0.2 dB/km]
+    :paramCh.D: chromatic dispersion parameter [ps/nm/km][default: 16 ps/nm/km]
+    :paramCh.gamma: fiber nonlinear parameter [1/W/km][default: 1.3 1/W/km]
+    :paramCh.Fc: carrier frequency [Hz] [default: 193.1e12 Hz]
+    :paramCh.amp: 'edfa', 'ideal', or 'None. [default:'edfa']
+    :paramCh.NF: edfa noise figure [dB] [default: 4.5 dB]    
+    
+    :return Ech: propagated signal
+    """
+    # check input parameters
+    paramCh.Ltotal = getattr(paramCh, 'Ltotal', 400)
+    paramCh.Lspan  = getattr(paramCh, 'Lspan', 80)
+    paramCh.hz     = getattr(paramCh, 'hz', 0.5)
+    paramCh.alpha  = getattr(paramCh, 'alpha', 0.2)
+    paramCh.D      = getattr(paramCh, 'D', 16)
+    paramCh.gamma  = getattr(paramCh, 'gamma', 1.3)
+    paramCh.Fc     = getattr(paramCh, 'Fc', 193.1e12)
+    paramCh.amp    = getattr(paramCh, 'amp', 'edfa')
+    paramCh.NF     = getattr(paramCh, 'NF', 4.5)   
+
+    Ltotal = paramCh.Ltotal 
+    Lspan  = paramCh.Lspan
+    hz     = paramCh.hz
+    alpha  = paramCh.alpha  
+    D      = paramCh.D      
+    gamma  = paramCh.gamma 
+    Fc     = paramCh.Fc     
+    amp    = paramCh.amp   
+    NF     = paramCh.NF
+
+    # channel parameters  
+    c_kms = const.c/1e3 # speed of light (vacuum) in km/s
+    λ  = c_kms/Fc
+    α  = alpha/(10*np.log10(np.exp(1)))
+    β2 = -(D*λ**2)/(2*np.pi*c_kms)
+    γ  = gamma
+
+    # generate frequency axis 
+    Nfft = len(Ei)
+    ω = 2*np.pi*Fs*fftfreq(Nfft)
+    
+    Nspans = int(np.floor(Ltotal/Lspan))
+    Nsteps = int(np.floor(Lspan/hz))
+    
+    Ech_x = Ei[:,0].reshape(len(Ei),)
+    Ech_y = Ei[:,1].reshape(len(Ei),) 
+
+    # define linear operator
+    linOperator = np.exp(-(α/2)*(hz/2) + 1j*(β2/2)*(ω**2)*(hz/2))
+    
+    for spanN in tqdm(range(1, Nspans+1)):   
+        Ech_x = fft(Ech_x) #polarization x field
+        Ech_y = fft(Ech_y) #polarization y field
+        
+        # fiber propagation step
+        for stepN in range(1, Nsteps+1):            
+            # First linear step (frequency domain)
+            Ech_x = Ech_x*linOperator
+            Ech_y = Ech_y*linOperator
+
+            # Nonlinear step (time domain)
+            Ech_x = ifft(Ech_x)
+            Ech_y = ifft(Ech_y)
+            Ech_x = Ech_x*np.exp(1j*(8/9)*γ*(Ech_x*np.conj(Ech_x)+Ech_y*np.conj(Ech_y))*hz)
+            Ech_y = Ech_y*np.exp(1j*(8/9)*γ*(Ech_x*np.conj(Ech_x)+Ech_y*np.conj(Ech_y))*hz)
+
+            # Second linear step (frequency domain)
+            Ech_x = fft(Ech_x)
+            Ech_y = fft(Ech_y)
+            
+            Ech_x = Ech_x*linOperator
+            Ech_y = Ech_y*linOperator
+            
+        # amplification step
+        Ech_x = ifft(Ech_x)
+        Ech_y = ifft(Ech_y)
+        
+        if amp =='edfa':
+            Ech_x = edfa(Ech_x, Fs, alpha*Lspan, NF, Fc)
+            Ech_y = edfa(Ech_y, Fs, alpha*Lspan, NF, Fc)
+        elif amp =='ideal':
+            Ech_x = Ech_x*np.exp(α/2*Nsteps*hz)
+            Ech_y = Ech_y*np.exp(α/2*Nsteps*hz)
+        elif amp == None:
+            Ech_x = Ech_x*np.exp(0);
+            Ech_y = Ech_y*np.exp(0);
+
+    Ech = np.array([Ech_x.reshape(len(Ei),),
+                    Ech_y.reshape(len(Ei),)]).T
+    
+    return Ech, paramCh
+
 def phaseNoise(lw, Nsamples, Ts):
     
     σ2 = 2*np.pi*lw*Ts    
