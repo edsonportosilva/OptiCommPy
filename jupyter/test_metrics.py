3c780d6210da86109cfeb9943b8f94b182b1d895
Edson Porto da Silva <edsonporto88@gmail.com>
2021-11-03 10:04:01 -0300

    update notebook

--- jupyter/test_metrics.py
+++ jupyter/test_metrics.py
@@ -7,9 +7,9 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.11.3
+#       jupytext_version: 1.13.0
 #   kernelspec:
-#     display_name: Python 3
+#     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
 # ---
@@ -210,7 +210,7 @@ for ii, M in enumerate(qamOrder):
 
         symbRx = awgn(symbTx, noiseVar)
 
-        # GMI estimation
+        # MI estimation
         MI[indSNR, ii] = monteCarloMI(symbRx, symbTx, constSymb, probSymb)
 
 # +
