d5d2e207d5b1476396cba5dafe6d669b3b902844
Edson Porto da Silva <edsonporto88@gmail.com>
2021-11-15 14:47:12 -0300

    Update carrierRecovery.py

--- optic/carrierRecovery.py
+++ optic/carrierRecovery.py
@@ -7,10 +7,89 @@ from tqdm.notebook import tqdm
 from optic.core import parameters
 
 
+
+def cpr(Ei, symbTx=[], param=[]):
+    """
+    Carrier phase recovery (CPR)
+
+    """
+    # check input parameters
+    alg        = getattr(param, 'alg', 'bps')
+    M          = getattr(param, 'M', 4)
+    B          = getattr(param, 'B', 16)
+    N          = getattr(param, 'N', 15)    
+    pilotInd   = getattr(param, 'pilotInd', [])
+    
+    try:
+        Ei.shape[1]
+    except IndexError:
+        Ei = Ei.reshape(len(Ei), 1)
+
+    mod = QAMModem(m=M)
+    constSymb = mod.constellation / np.sqrt(mod.Es)
+
+    if alg == 'ddpll':
+        ϕ, θ = ddpll(Ei, N, constSymb, symbTx, pilotInd)      
+        Eo = Ei*np.exp(1j*np.unwrap(4*θ, axis=0)/4)
+
+    elif alg == 'bps':
+        θ = bps(Ei, N, constSymb, B)
+        ϕ = θ.copy()
+        Eo = Ei*np.exp(1j*np.unwrap(4*θ, axis=0)/4)
+    else:
+        raise ValueError('CPR algorithm not specified (or incorrectly specified).')
+
+    if Eo.shape[1] == 1:
+        Eo = Eo[:]
+        ϕ = ϕ[:]
+        θ = θ[:]
+
+    return Eo, ϕ, θ
+
+
+@njit
+def bps(Ei, N, constSymb, B):
+    """
+    blind phase search (BPS) algorithm  
+    """
+    nModes = Ei.shape[1]
+
+    ϕ_test = np.arange(0, B) * (np.pi / 2) / B  # test phases
+
+    θ = np.zeros(Ei.shape, dtype="float")
+
+    zeroPad = np.zeros((N, nModes), dtype="complex")
+    x = np.concatenate(
+        (zeroPad, Ei, zeroPad)
+    )  # pad start and end of the signal with zeros
+
+    L = x.shape[0]
+
+    for n in range(0, nModes):
+
+        dist = np.zeros((B, constSymb.shape[0]), dtype="float")
+        dmin = np.zeros((B, 2 * N + 1), dtype="float")
+
+        for k in range(0, L):
+
+            for indPhase, ϕ in enumerate(ϕ_test):
+                dist[indPhase, :] = np.abs(x[k, n]*np.exp(1j * ϕ) - constSymb) ** 2
+                dmin[indPhase, -1] = np.min(dist[indPhase, :])
+
+            if k >= 2 * N:
+                sumDmin = np.sum(dmin, axis=1)
+                indRot = np.argmin(sumDmin)
+                θ[k - 2 * N, n] = ϕ_test[indRot]
+
+            dmin = np.roll(dmin, -1)
+
+    return θ
+
+
 @njit
 def ddpll(Ei, N, constSymb, symbTx, pilotInd):
     """
-    DDPLL
+    decision directed phase-locked loop (DDPLL)
     """
     nModes = Ei.shape[1]
 
@@ -42,65 +121,5 @@ def ddpll(Ei, N, constSymb, symbTx, pilotInd):
             else:
                 θ[k, n] = np.angle(symbTx[k, n] / (Ei[k, n]))
 
-    Eo = Ei * np.exp(1j * θ)  # compensate phase rotation
 
-    return Eo, ϕ, θ
-
-
-def cpr(Ei, N, M, symbTx, pilotInd=[]):
-    """
-    Carrier phase recovery (CPR)
-
-    """
-    try:
-        Ei.shape[1]
-    except IndexError:
-        Ei = Ei.reshape(len(Ei), 1)
-
-    mod = QAMModem(m=M)
-    constSymb = mod.constellation / np.sqrt(mod.Es)
-    if alg == 'ddpll':
-        Eo, ϕ, θ = ddpll(Ei, N, constSymb, symbTx, pilotInd)
-    elif: 
-         Eo, ϕ, θ = ddpll(Ei, N, constSymb, symbTx, pilotInd)
-         
-    if Eo.shape[1] == 1:
-        Eo = Eo[:]
-        ϕ = ϕ[:]
-        θ = θ[:]
-
-    return Eo, ϕ, θ
-
-
-@njit
-def bps(Ei, constSymb, B, N):
-    """
-    blind phase search (BPS) algorithm  
-    """          
-    ϕ_test = np.arange(0, B)*(np.pi/2)/B # test phases
-    
-    dist = np.zeros((constSymb.shape[0], B), dtype=np.float)
-    L = Ei.shape[0]
-    
-    ϕ_dec = np.zeros(L, dtype=np.float)
-    θ = np.zeros(L, dtype=np.float)
-    
-    for k in range(0, L):                
-        for indPhase, ϕ in enumerate(ϕ_test):     
-            
-            dist[:, indPhase] = np.abs(Ei[k]-constSymb*np.exp(1j*ϕ))
-        
-        indRot = np.argwhere( dist == np.min(dist) )[0, 1]
-        
-        ϕ_dec[k] = ϕ_test[indRot]
-        
-        if k > 2*N+1:
-            θ[k] = np.mean(ϕ_dec[k - N: k + N])  # moving average filter
-        else:
-            θ[k] = ϕ_dec[k]
-            
-    Eo = Ei * np.exp(1j * θ)  # compensate phase rotation
-        
-    return Eo, θ, ϕ
-
-
+    return ϕ, θ
