fadef8826e209dd1e72fc69467f13bc47312d23c
Edson Porto da Silva <edsonporto88@gmail.com>
2021-11-23 17:32:43 -0300

    Update fec.py

--- optic/fec.py
+++ optic/fec.py
@@ -5,90 +5,134 @@ Created on Sat Nov 20 19:23:45 2021
 @author: edson
 """
 import numpy as np
-from numba import njit
+from commpy.channelcoding import ldpc
+from commpy.channelcoding.ldpc import triang_ldpc_systematic_encode as enc
+from commpy.channelcoding.ldpc import ldpc_bp_decode as dec
 
-@njit
-def loggaldecode(H, r, Nloop, Lc, Nl, Ml):
-    # function x = loggaldecode(A,r,Nloop,Lc)
-    #
-    # Do log-likelihood decoding on a low-density parity check code
-    # H = parity check matrix
-    # r = received signal vector
-    # Nloop = number of iterations
-    # Lc = channel reliability
-    # Nl = Nl set of sparse representation
-    # Ml = Ml set of sparse representation
 
-    # Copyright 2004 by Todd K. Moon
-    # Permission is granted to use this program/data
-    # for educational/research only
+def ldpcEncode(b, filePath):
+    """
+    Encode data bits with binary LDPC code
+    b = np.random.randint(2, size=(K, Nwords))
 
-    M, N = H.shape
-    H = H.astype(np.float64)
+    """
+    LDPCparams = ldpc.get_ldpc_code_params(filePath)
 
-    # Nl = []
-    # Ml = []
-    # for m in range(M):
-    #     Nl.append([])
+    N = LDPCparams["n_vnodes"]
 
-    # for n in range(N):
-    #     Ml.append([])
+    # generate random interleaver
+    interlv = np.random.permutation(N)
 
-    # # Build the sparse representation of A using the M and N sets
+    # encode bits
+    codedBits = enc(b, LDPCparams)
+    interCodedBits = (codedBits[interlv, :].T).reshape(1, -1).T
 
-    # for m in range(M):
-    #     for n in range(N):
-    #         if H[m, n]:
-    #             Nl[m].append(n)
-    #             Ml[n].append(m)
+    return interCodedBits, codedBits, interlv, LDPCparams
 
-    # Initialize the probabilities
-    eta = np.zeros((M, N), dtype=np.float64)
-    lasteta = np.zeros((M, N), dtype=np.float64)
-    pr = np.zeros(1, dtype=np.float64)
-    lamb = Lc * r   
 
-    for loop in range(Nloop):
-        
-        for m in range(M):  # for each row (check)
-            for n in Nl[m]:  # work across the columns ("horizontally")
-                pr[:] = 1.0
-                for pn in Nl[m]:
-                    if pn == n:
-                        continue
-                    pr[:] *= np.tanh(
-                        (-lamb[pn] + lasteta[m, pn]) / 2
-                    )  # accumulate the product
+def ldpcDecode(llr, interlv, LDPCparams, nIter, alg="SPA"):
+    """
+    Decode binary LDPC encoded data bits
+    b = np.random.randint(2, size=(K, Nwords))
 
-                eta[m, n] = -2 * np.arctanh(pr[0])
-                eta[m, n] = max([eta[m, n], -100.0])
-                eta[m, n] = min([eta[m, n],  100.0])
-         
-                    
-        lasteta = eta  # save to subtract to obtain extrinsic for next time around
-        # fprintf(1,'eta:'); splatexform(1,eta,1);
+    """
+    N = LDPCparams["n_vnodes"]
 
-        for n in range(N):  # for each column (bit)
-            lamb[n] = Lc * r[n]
+    # generate deinterleaver
+    deinterlv = interlv.argsort()
 
-            for m in Ml[n]:  # work down the rows ("vertically")
-                lamb[n] = lamb[n] + eta[m, n]
-            
-            if lamb[n] > 100:
-                lamb[n] = 100.0
-            elif lamb[n] < -100:
-                lamb[n] = -100.0
-   
-        x = ((np.sign(lamb) + 1) / 2 ).astype(np.float64)  # compute decoded bits for stopping criterion
-        z1 = ((H @ x) % 2).T
+    # deinterleave received LLRs
+    llr = llr.reshape(-1, N)
+    llr = llr[:, deinterlv].ravel()
 
-        if np.all(z1.astype(np.int32) == 0):
-            break
-    # end for loop
+    # decode received code words
+    decodedBits, llr_out = dec(llr, LDPCparams, alg, nIter)
 
-    # if ~np.all(z1 == 0):
-    #     print("Decoding failure after %d iterations" % Nloop)
-    # else:
-    #     print("Successfull Decoding after %d iterations" % (loop + 1))
+    return decodedBits, llr_out
 
-    return lamb, x
+
+# @njit
+# def loggaldecode(H, r, Nloop, Lc, Nl, Ml):
+#     # function x = loggaldecode(A,r,Nloop,Lc)
+#     #
+#     # Do log-likelihood decoding on a low-density parity check code
+#     # H = parity check matrix
+#     # r = received signal vector
+#     # Nloop = number of iterations
+#     # Lc = channel reliability
+#     # Nl = Nl set of sparse representation
+#     # Ml = Ml set of sparse representation
+
+#     # Copyright 2004 by Todd K. Moon
+#     # Permission is granted to use this program/data
+#     # for educational/research only
+
+#     M, N = H.shape
+#     H = H.astype(np.float64)
+
+#     # Nl = []
+#     # Ml = []
+#     # for m in range(M):
+#     #     Nl.append([])
+
+#     # for n in range(N):
+#     #     Ml.append([])
+
+#     # # Build the sparse representation of A using the M and N sets
+
+#     # for m in range(M):
+#     #     for n in range(N):
+#     #         if H[m, n]:
+#     #             Nl[m].append(n)
+#     #             Ml[n].append(m)
+
+#     # Initialize the probabilities
+#     eta = np.zeros((M, N), dtype=np.float64)
+#     lasteta = np.zeros((M, N), dtype=np.float64)
+#     pr = np.zeros(1, dtype=np.float64)
+#     lamb = r
+
+#     for loop in range(Nloop):
+
+#         for m in range(M):  # for each row (check)
+#             for n in Nl[m]:  # work across the columns ("horizontally")
+#                 pr[:] = 1.0
+#                 for pn in Nl[m]:
+#                     if pn == n:
+#                         continue
+#                     pr[:] *= np.tanh(
+#                         (-lamb[pn] + lasteta[m, pn]) / 2
+#                     )  # accumulate the product
+
+#                 eta[m, n] = -2 * np.arctanh(pr[0])
+#                 eta[m, n] = max([eta[m, n], -500.0])
+#                 eta[m, n] = min([eta[m, n],  500.0])
+
+
+#         lasteta = eta  # save to subtract to obtain extrinsic for next time around
+#         # fprintf(1,'eta:'); splatexform(1,eta,1);
+#         lamb = r
+#         for n in range(N):  # for each column (bit)
+#             #lamb[n] = Lc * r[n]
+
+#             for m in Ml[n]:  # work down the rows ("vertically")
+#                 lamb[n] = lamb[n] + eta[m, n]
+
+#                 if lamb[n] >= 500:
+#                     lamb[n] = 500.0
+#                 elif lamb[n] <= -500:
+#                     lamb[n] = -500.0
+
+#         x = ((np.sign(lamb) + 1) / 2 ).astype(np.float64)  # compute decoded bits for stopping criterion
+#         z1 = ((H @ x) % 2).T
+
+#         if np.all(z1.astype(np.int32) == 0):
+#             break
+#     # end for loop
+
+#     # if ~np.all(z1 == 0):
+#     #     print("Decoding failure after %d iterations" % Nloop)
+#     # else:
+#     #     print("Successfull Decoding after %d iterations" % (loop + 1))
+
+#     return lamb, x
