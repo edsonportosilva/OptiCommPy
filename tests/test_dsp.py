7c1f6cd5954319b4aa72006ce986e46e6ef9cbed
Edson Porto da Silva <edsonporto88@gmail.com>
2022-02-19 13:15:21 -0300

    Update test_dsp.py

--- tests/test_dsp.py
+++ tests/test_dsp.py
@@ -5,3 +5,23 @@ Created on Sat Feb 19 12:58:47 2022
 @author: edson
 """
 
+from optic.dsp import finddelay
+import numpy as np
+
+
+def test_finddelay_for_arrays_of_real_values():
+    delay = 35
+
+    a = np.arange(0, 100)
+    b = np.roll(a, -delay)
+
+    assert delay == finddelay(a, b)
+
+
+def test_finddelay_for_arrays_of_complex_values():
+    delay = 57
+
+    a = np.arange(0, 100) + 1j * np.arange(0, 100)
+    b = np.roll(a, -delay)
+
+    assert delay == finddelay(a, b)
