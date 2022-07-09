f83239a237adcee5bd07aa4f167552fc61ef1f10
Edson Porto da Silva <edsonporto88@gmail.com>
2022-07-09 13:59:32 -0300

    Create modulation.py

---
+++ optic/modulation.py
@@ -0,0 +1,67 @@
+import numpy as np
+from numpy.matlib import repmat
+from commpy.utilities import bitarray2dec
+
+def GrayCode(n):
+    
+    code = []
+    
+    for i in range(1 << n):
+       
+        # Generating the decimal
+        # values of gray code then using
+        # bitset to convert them to binary form
+        val = (i ^ (i >> 1))
+         
+        # Converting to binary string
+        s = bin(val)[2::]
+        code.append(s.zfill(n))
+        
+    return code
+
+def GrayMapping(M, constType):
+    
+    L   = int(np.sqrt(M)-1)
+    bitsSymb = int(np.log2(M))
+    
+    code = GrayCode(bitsSymb)
+        
+    if constType == 'qam':
+        PAM = np.arange(-L, L+1, 2)
+        PAM = np.array([PAM])
+
+        # generate complex square M-QAM constellation
+        const = repmat(PAM, L+1, 1) + 1j*repmat(np.flip(PAM.T,0), 1, L+1)
+        const = const.T
+    
+        for ind in np.arange(1,L+1,2):
+            const[ind] = np.flip(const[ind],0)        
+        
+    elif constType == 'psk':
+        pskPhases = np.arange(0,2*np.pi,2*np.pi/M)
+        
+        # generate complex M-PSK constellation
+        const     = np.exp(1j*pskPhases) 
+    
+    const    = const.reshape(M,1)
+    const_   = np.zeros((M,2),dtype=complex)
+    
+
+    for ind in range(0,M):    
+        const_[ind,0]   = const[ind,0]   # complex constellation symbol
+        const_[ind,1]   =  int(code[ind],2) # mapped bit sequence (as integer decimal)
+        
+    # sort complex symbols column according to their mapped bit sequence (as integer decimal)                 
+    const = const_[const_[:,1].real.argsort()] 
+    
+    return const
+
+def modulateGray(bits, M, constType):
+       
+    bitsSymb = int(np.log2(M))      
+    const = GrayMapping(M, constType)
+    
+    symb    = bits.reshape(-1,bitsSymb).T
+    symbInd = bitarray2dec(symb)    
+
+    return const[symbInd,0]
