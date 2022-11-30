74145b7cb0ace67d3d5c328e1a6b95af8048be8c
Adolfo Herbster <adophobr@gmail.com>
2022-11-30 08:50:40 -0300

    Update amplification.py

--- optic/amplification.py
+++ optic/amplification.py
@@ -74,19 +74,19 @@ def OSA(x, Fs, Fc=193.1e12):
     plot
 
     """
-    ZX, freqs = getSpectrum(x[:,0], Fs, Fc)
-    lmbd = 1e9 * c / freqs
-    plt.figure()
-    plt.plot(lmbd, ZX, label="X Pol.")
+    if (np.shape(x)[1] == 1):    # plot X
+        ZX, freqs = getSpectrum(x[:,0], Fs, Fc)
+        plt.plot( 1e9 * c / freqs, ZX, label="X Pol.")
+        yMin = -70
+        plt.ylim([yMin, ZX.max() + 10])
+    elif (np.shape(x)[1] == 2):  # plot Y
+        ZY, freqs = getSpectrum(x[:,1], Fs, Fc)
+        plt.plot( 1e9 * c / freqs, ZY, label="Y Pol.", alpha=0.5)
+        plt.ylim([yMin, np.array([ZX.max(), ZY.max()]).max() + 10])
     plt.xlabel("Frequency [nm]")
     plt.ylabel("Magnitude [dBm]")
     plt.grid(True)
-    plt.ylim([-70, ZX.max() + 10])
-    if (np.shape(x)[1] == 2):
-        ZY, freqs = getSpectrum(x[:,1], Fs, Fc)
-        plt.plot(lmbd, ZY, label="Y Pol.", alpha=0.5)
-        plt.legend()
-        plt.ylim([-70, np.array([ZX.max(), ZY.max()]).max() + 10])   
+    plt.legend()
     
     return
 
