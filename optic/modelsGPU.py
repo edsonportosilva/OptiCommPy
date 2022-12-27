b45169debcf274a769aa5f36432c7bbfbf7f424a
Edson Porto da Silva <edsonporto88@gmail.com>
2022-12-26 21:07:35 -0300

    Update modelsGPU.py

--- optic/modelsGPU.py
+++ optic/modelsGPU.py
@@ -52,13 +52,14 @@ def edfa(Ei, Fs, G=20, NF=4.5, Fc=193.1e12, prec=cp.complex128):
 
 def convergenceCondition(Ex_fd, Ey_fd, Ex_conv, Ey_conv):
 
-    return cp.sqrt(norm(Ex_fd - Ex_conv) ** 2 + 
-                   norm(Ey_fd - Ey_conv) ** 2) / cp.sqrt(norm(Ex_conv) ** 2 + norm(Ey_conv) ** 2
-    )
+    return cp.sqrt(
+        norm(Ex_fd - Ex_conv) ** 2 + norm(Ey_fd - Ey_conv) ** 2
+    ) / cp.sqrt(norm(Ex_conv) ** 2 + norm(Ey_conv) ** 2)
+
 
 def ssfm(Ei, Fs, paramCh, prec=cp.complex128):
     """
-    Run the split-step Fourier model (symmetric, dual-pol.).
+    Run the split-step Fourier model (symmetric, single-pol.).
 
     Parameters
     ----------
@@ -78,7 +79,8 @@ def ssfm(Ei, Fs, paramCh, prec=cp.complex128):
     paramCh.Fc: carrier frequency [Hz] [default: 193.1e12 Hz]
     paramCh.amp: 'edfa', 'ideal', or 'None. [default:'edfa']
     paramCh.NF: edfa noise figure [dB] [default: 4.5 dB]
-    paramCh.prgsBar: display progress bar? bolean variable [default:True]
+    paramCh.prgsBar: display progress bar? bolean variable [default:True]    
+    paramCh.saveSpanN: specify the span indexes to be output [default:[]]
 
     Returns
     -------
@@ -98,11 +100,8 @@ def ssfm(Ei, Fs, paramCh, prec=cp.complex128):
     paramCh.Fc = getattr(paramCh, "Fc", 193.1e12)
     paramCh.amp = getattr(paramCh, "amp", "edfa")
     paramCh.NF = getattr(paramCh, "NF", 4.5)
-    paramCh.maxIter = getattr(paramCh, "maxIter", 10)
-    paramCh.tol = getattr(paramCh, "tol", 1e-5)
-    paramCh.prgsBar = getattr(paramCh, "prgsBar", True)
-    paramCh.recordSpans = getattr(paramCh, "recordSpans", False)
-    paramCh.toBeRecorded = getattr(paramCh, "toBeRecorded", [])
+    paramCh.prgsBar = getattr(paramCh, "prgsBar", True)    
+    paramCh.saveSpanN = getattr(paramCh, "saveSpanN", [int(paramCh.Ltotal/paramCh.Lspan)])
 
     Ltotal = paramCh.Ltotal
     Lspan = paramCh.Lspan
@@ -113,11 +112,8 @@ def ssfm(Ei, Fs, paramCh, prec=cp.complex128):
     Fc = paramCh.Fc
     amp = paramCh.amp
     NF = paramCh.NF
-    maxIter = paramCh.maxIter
-    tol = paramCh.tol
     prgsBar = paramCh.prgsBar
-    recordSpans = paramCh.recordSpans
-    toBeRecorded = paramCh.toBeRecorded
+    saveSpanN = paramCh.saveSpanN
 
     Nspans = int(np.floor(Ltotal / Lspan))
     Nsteps = int(np.floor(Lspan / hz))
@@ -126,7 +122,7 @@ def ssfm(Ei, Fs, paramCh, prec=cp.complex128):
     c_kms = const.c / 1e3  # speed of light (vacuum) in km/s
     λ = c_kms / Fc
     α = alpha / (10 * np.log10(np.exp(1)))
-    β2 = -(D * λ ** 2) / (2 * np.pi * c_kms)
+    β2 = -(D * λ**2) / (2 * np.pi * c_kms)
     γ = gamma
 
     c_kms = cp.asarray(c_kms, dtype=prec)  # speed of light (vacuum) in km/s
@@ -148,14 +144,13 @@ def ssfm(Ei, Fs, paramCh, prec=cp.complex128):
     )
     # define linear operator
     linOperator = cp.array(
-        cp.exp(-(α / 2) * (hz / 2) + 1j * (β2 / 2) * (ω ** 2) * (hz / 2))
+        cp.exp(-(α / 2) * (hz / 2) + 1j * (β2 / 2) * (ω**2) * (hz / 2))
     ).astype(prec)
-    
-    if recordSpans:
-        Ech_spans = cp.zeros((Ei_.shape[0], Ei_.shape[1] * len(toBeRecorded))).astype(
-            prec
-        )
-        
+  
+    Ech_spans = cp.zeros(
+        (Ech.shape[0], len(saveSpanN))
+    ).astype(prec)
+
     indRecSpan = 0
 
     for spanN in tqdm(range(1, Nspans + 1), disable=not (prgsBar)):
@@ -182,22 +177,20 @@ def ssfm(Ei, Fs, paramCh, prec=cp.complex128):
             Ech = Ech * cp.exp(α / 2 * Nsteps * hz)
         elif amp is None:
             Ech = Ech * cp.exp(0)
-            
-        if recordSpans and spanN in toBeRecorded:
-            Ech_spans[:, indRecSpan] = Ech            
+
+        if spanN in saveSpanN:
+            Ech_spans[:, indRecSpan] = Ech
             indRecSpan += 1
+
+    
+    Ech = cp.asnumpy(Ech_spans)
+    
+    if Ech.shape[1] == 1:
+        Ech = Ech.reshape(
+                    len(Ech),)
         
-    if recordSpans:
-        Ech = cp.asnumpy(Ech_spans)
-    else:
-        Ech = cp.asnumpy(Ech)
-            
-    return (
-        Ech.reshape(
-            len(Ech),
-        ),
-        paramCh,
-    )
+    return Ech, paramCh
+
 
 def manakovSSF(Ei, Fs, paramCh, prec=cp.complex128):
     """
@@ -221,7 +214,8 @@ def manakovSSF(Ei, Fs, paramCh, prec=cp.complex128):
     paramCh.Fc: carrier frequency [Hz] [default: 193.1e12 Hz]
     paramCh.amp: 'edfa', 'ideal', or 'None. [default:'edfa']
     paramCh.NF: edfa noise figure [dB] [default: 4.5 dB]
-    paramCh.prgsBar: display progress bar? bolean variable [default:True]
+    paramCh.prgsBar: display progress bar? bolean variable [default:True]    
+    paramCh.saveSpanN: specify the span indexes to be output [default:[]]
 
     Returns
     -------
@@ -243,9 +237,8 @@ def manakovSSF(Ei, Fs, paramCh, prec=cp.complex128):
     paramCh.NF = getattr(paramCh, "NF", 4.5)
     paramCh.maxIter = getattr(paramCh, "maxIter", 10)
     paramCh.tol = getattr(paramCh, "tol", 1e-5)
-    paramCh.prgsBar = getattr(paramCh, "prgsBar", True)
-    paramCh.recordSpans = getattr(paramCh, "recordSpans", False)
-    paramCh.toBeRecorded = getattr(paramCh, "toBeRecorded", [])
+    paramCh.prgsBar = getattr(paramCh, "prgsBar", True)    
+    paramCh.saveSpanN = getattr(paramCh, "saveSpanN", [int(paramCh.Ltotal/paramCh.Lspan)])
 
     Ltotal = paramCh.Ltotal
     Lspan = paramCh.Lspan
@@ -259,8 +252,7 @@ def manakovSSF(Ei, Fs, paramCh, prec=cp.complex128):
     maxIter = paramCh.maxIter
     tol = paramCh.tol
     prgsBar = paramCh.prgsBar
-    recordSpans = paramCh.recordSpans
-    toBeRecorded = paramCh.toBeRecorded
+    saveSpanN = paramCh.saveSpanN
 
     Nspans = int(np.floor(Ltotal / Lspan))
     Nsteps = int(np.floor(Lspan / hz))
@@ -269,7 +261,7 @@ def manakovSSF(Ei, Fs, paramCh, prec=cp.complex128):
     c_kms = const.c / 1e3  # speed of light (vacuum) in km/s
     λ = c_kms / Fc
     α = alpha / (10 * np.log10(np.exp(1)))
-    β2 = -(D * λ ** 2) / (2 * np.pi * c_kms)
+    β2 = -(D * λ**2) / (2 * np.pi * c_kms)
     γ = gamma
 
     c_kms = cp.asarray(c_kms, dtype=prec)  # speed of light (vacuum) in km/s
@@ -291,17 +283,17 @@ def manakovSSF(Ei, Fs, paramCh, prec=cp.complex128):
 
     # define linear operator
     linOperator = cp.array(
-        cp.exp(-(α / 2) * (hz / 2) + 1j * (β2 / 2) * (ω ** 2) * (hz / 2))
+        cp.exp(-(α / 2) * (hz / 2) + 1j * (β2 / 2) * (ω**2) * (hz / 2))
     ).astype(prec)
 
     if Ech_x.shape[0] > 1:
         linOperator = cp.tile(linOperator, (Ech_x.shape[0], 1))
     else:
         linOperator = linOperator.reshape(1, -1)
-    if recordSpans:
-        Ech_spans = cp.zeros((Ei_.shape[0], Ei_.shape[1] * len(toBeRecorded))).astype(
-            prec
-        )
+    if saveSpanN:
+        Ech_spans = cp.zeros(
+            (Ei_.shape[0], Ei_.shape[1] * len(saveSpanN))
+        ).astype(prec)
         indRecSpan = 0
     for spanN in tqdm(range(1, Nspans + 1), disable=not (prgsBar)):
 
@@ -322,7 +314,11 @@ def manakovSSF(Ei, Fs, paramCh, prec=cp.complex128):
                 phiRot = (
                     (8 / 9)
                     * γ
-                    * (Pch + Ex_conv * cp.conj(Ex_conv) + Ey_conv * cp.conj(Ey_conv))
+                    * (
+                        Pch
+                        + Ex_conv * cp.conj(Ex_conv)
+                        + Ey_conv * cp.conj(Ey_conv)
+                    )
                     * hz
                     / 2
                 )
@@ -335,7 +331,9 @@ def manakovSSF(Ei, Fs, paramCh, prec=cp.complex128):
                 Ech_y_fd = ifft(fft(Ech_y_fd) * linOperator)
 
                 # check convergence o trapezoidal integration in phiRot
-                lim = convergenceCondition(Ech_x_fd, Ech_y_fd, Ex_conv, Ey_conv)
+                lim = convergenceCondition(
+                    Ech_x_fd, Ech_y_fd, Ex_conv, Ey_conv
+                )
 
                 Ex_conv = Ech_x_fd.copy()
                 Ey_conv = Ech_y_fd.copy()
@@ -343,7 +341,9 @@ def manakovSSF(Ei, Fs, paramCh, prec=cp.complex128):
                 if lim < tol:
                     break
                 elif nIter == maxIter - 1:
-                    logg.warning( f"Warning: target SSFM error tolerance was not achieved in {maxIter} iterations")
+                    logg.warning(
+                        f"Warning: target SSFM error tolerance was not achieved in {maxIter} iterations"
+                    )
 
             Ech_x = Ech_x_fd.copy()
             Ech_y = Ech_y_fd.copy()
@@ -358,12 +358,12 @@ def manakovSSF(Ei, Fs, paramCh, prec=cp.complex128):
         elif amp is None:
             Ech_x = Ech_x * cp.exp(0)
             Ech_y = Ech_y * cp.exp(0)
-        if recordSpans and spanN in toBeRecorded:
-            Ech_spans[:, 2 * indRecSpan: 2 * indRecSpan + 1] = Ech_x.T
-            Ech_spans[:, 2 * indRecSpan + 1: 2 * indRecSpan + 2] = Ech_y.T
+        if spanN in saveSpanN:
+            Ech_spans[:, 2 * indRecSpan : 2 * indRecSpan + 1] = Ech_x.T
+            Ech_spans[:, 2 * indRecSpan + 1 : 2 * indRecSpan + 2] = Ech_y.T
             indRecSpan += 1
 
-    if recordSpans:
+    if saveSpanN:
         Ech = cp.asnumpy(Ech_spans)
     else:
         Ech_x = cp.asnumpy(Ech_x)
@@ -384,7 +384,8 @@ def setPowerforParSSFM(sig, powers):
     for i in np.arange(0, sig.shape[1], 2):
         for k in range(2):
             sig[:, i + k] = (
-                np.sqrt(powers_lin[i] / signal_power(sig[:, i + k])) * sig[:, i + k]
+                np.sqrt(powers_lin[i] / signal_power(sig[:, i + k]))
+                * sig[:, i + k]
             )
             print(
                 "power mode %d: %.2f dBm"
