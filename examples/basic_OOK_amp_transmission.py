54429d8453f58aac22cf1d16cf4097792d6eccde
Edson Porto da Silva <edsonporto88@gmail.com>
2023-06-15 16:07:41 -0300

    Update basic_OOK_amp_transmission.py

--- examples/basic_OOK_amp_transmission.py
+++ examples/basic_OOK_amp_transmission.py
@@ -1,37 +1,48 @@
-# %% [markdown]
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.8
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
 # <a href="https://colab.research.google.com/github/edsonportosilva/OptiCommPy/blob/main/jupyter/basic_OOK_transmission.ipynb" target="_parent"><img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/></a>
 
-# %% [markdown]
-# # Simulate a basic OOK transmission system
+# # Simulate a OOK transmission system with an advanced EDFA model
 
-# %%
 if 'google.colab' in str(get_ipython()):    
-    ! git clone -b main https://github.com/edsonportosilva/OptiCommPy
+    # ! git clone -b main https://github.com/edsonportosilva/OptiCommPy
     from os import chdir as cd
     cd('/content/OptiCommPy/')
-    ! pip install . 
+    # ! pip install . 
 
-# %%
+# +
 import numpy as np
 from commpy.utilities  import upsample
-from optic.models import mzm, photodiode
-from optic.metrics import signal_power
-from optic.dsp import firFilter, pulseShape
+from optic.models.devices import mzm, photodiode
+from optic.dsp.core import signal_power, firFilter, pulseShape
 from optic.core import parameters
 from optic.plot import eyediagram
 import matplotlib.pyplot as plt
 from scipy.special import erfc
 
 from scipy.constants import c
-from optic.amplification import edfaSM,OSA
+from optic.models.amplification import edfaSM,OSA
 
 import os.path as path
+# -
 
-#import logging as logg
-#logg.getLogger().setLevel(logg.INFO)
-#logg.basicConfig(format='%(message)s')
+import logging as logg
+logg.getLogger().setLevel(logg.INFO)
+logg.basicConfig(format='%(message)s')
 
-# %%
 from IPython.core.display import HTML
 from IPython.core.pylabtools import figsize
 
@@ -45,15 +56,13 @@ HTML("""
 </style>
 """)
 
-# %%
 figsize(10, 3)
 
-# %% [markdown]
 # ### Intensity modulation (IM) with On-Off Keying (OOK)
 
-# %%
+# +
 # simulation parameters
-SpS = 16
+SpS = 8
 
 Rs = 10e9 # Symbol rate (for OOK case Rs = Rb)
 Tsymb = 1/Rs # Symbol period in seconds
@@ -92,7 +101,6 @@ sigTxo = mzm(Ai, sigTx, Vπ, Vb)
 print('Average power of the modulated optical signal [mW]: %.3f mW'%(signal_power(sigTxo)/1e-3))
 print('Average power of the modulated optical signal [dBm]: %.3f dBm'%(10*np.log10(signal_power(sigTxo)/1e-3)))
 
-# %%
 fig, axs = plt.subplots(1, 2, figsize=(16,3))
 interval = np.arange(16*20,16*50)
 t = interval*Ts/1e-9
@@ -123,14 +131,13 @@ axs[1].set_xlabel('Time (ns)')
 axs[1].set_xlim(min(t),max(t))
 axs[1].legend(loc='upper left')
 axs[1].grid()
+# -
 
-# %% [markdown]
 # ## Without optical amplification
 
-# %% [markdown]
 # ### Direct-detection (DD) pin receiver model
 
-# %%
+# +
 # ideal photodiode (noiseless, no bandwidth limitation)
 paramPD = parameters()
 paramPD.ideal = True
@@ -149,7 +156,6 @@ discard = 100
 eyediagram(I_Rx_ideal[discard:-discard].copy(), I_Rx.size-2*discard, SpS, plotlabel='signal at Tx', ptype='fancy')
 eyediagram(I_Rx[discard:-discard].copy(), I_Rx.size-2*discard, SpS, plotlabel='signal at Rx', ptype='fancy')
 
-# %%
 I_Rx = I_Rx/np.std(I_Rx)
 
 # capture samples in the middle of signaling intervals
@@ -172,6 +178,7 @@ print('σ1 = %.2f '%(σ1))
 print('Optimal decision threshold Id = %.2f '%(Id))
 print('Q = %.2f \n'%(Q))
 
+# +
 # Apply the optimal decision rule
 bitsRx = np.empty(bitsTx.size)
 bitsRx[I_Rx> Id] = 1
@@ -197,11 +204,11 @@ plt.legend()
 plt.grid()
 plt.ylim(0, 1.5)
 plt.xlim(0,err.size);
+# -
 
-# %% [markdown]
 # ### Optical amplification
 
-# %%
+# +
 # EDFA parameters
 param_edfa = parameters()
 # gain control
@@ -223,13 +230,11 @@ param_edfa.tolCtrl  = 0.5
 # ASE
 param_edfa.noiseBand= 1.25e9
 
-# %%
 if 'google.colab' in str(get_ipython()):  
-  param_edfa.file = path.join(path.abspath(path.join("../")), 'OptiCommPy', 'optic', 'ampParams', param_edfa.file)
+  param_edfa.file = path.join(path.abspath(path.join("../")), 'OptiCommPy', 'optic', 'models', 'ampParams', param_edfa.file)
 else:
-  param_edfa.file = path.join(path.abspath(path.join("../")), 'optic', 'ampParams', param_edfa.file)
+  param_edfa.file = path.join(path.abspath(path.join("../")), 'optic', 'models', 'ampParams', param_edfa.file)
 
-# %%
 Fc = c/1550e-9 # central wavelength
 Nmodes = 1     # number of modes
 sigTxo = np.reshape(sigTxo, (len(sigTxo), 1))
@@ -237,7 +242,6 @@ lenFrqSg, isy = np.shape(sigTxo)
 simOpticalBand = (Fs*(c/Fc)**2)/c
 tx_pw = np.sum(1000*np.mean(sigTxo * np.conj(sigTxo), axis = 0).real)
 
-# %%
 # information TX
 print('Sample rate [THz]: %5.3f' %(1e-12*Fs))
 print('Time window [ns]:  %5.3f' %(1e9*lenFrqSg/Fs))
@@ -250,13 +254,12 @@ print('Number of modes: %d' %(Nmodes))
 print('Average power - TX [mW] : %.3f mW' %(tx_pw))
 print('Average power - TX [dBm] : %.3f dBm' %(10*np.log10(tx_pw)))
 
-# %%
-OSA(sigTxo, Fs, Fc)
+OSA(sigTxo, Fs, Fc);
+# -
 
-# %%
 Eout, PumpF, PumpB, noisef = edfaSM(sigTxo, Fs, Fc, param_edfa)
 
-# %%
+# +
 rx_pw = np.sum(1000*np.mean(Eout * np.conj(Eout), axis = 0).real)
 print('Forward pump  - [mW] : %.3f' %(1e3*PumpF[0]))
 print('Backward pump - [mW] : %.3f' %(1e3*PumpB[1]))
@@ -264,10 +267,9 @@ print('Average power - RX amp [mW] : %.3f' %(rx_pw))
 print('Average power - RX amp [dBm] : %.3f' %(10*np.log10(rx_pw)))
 print('Gain [dB]: %.3f' %(10*np.log10(rx_pw/tx_pw)))
 
-# %%
-OSA(Eout, Fs, Fc)
+OSA(Eout, Fs, Fc);
 
-# %%
+# +
 # ideal photodiode (noiseless, no bandwidth limitation)
 paramPD = parameters()
 paramPD.ideal = True
@@ -286,7 +288,6 @@ discard = 100
 eyediagram(I_Rx_Amp_ideal[discard:-discard].copy(), I_Rx_Amp.size-2*discard, SpS, plotlabel='signal at Tx', ptype='fancy')
 eyediagram(I_Rx_Amp[discard:-discard].copy(), I_Rx_Amp.size-2*discard, SpS, plotlabel='signal at Rx', ptype='fancy')
 
-# %%
 I_Rx_Amp = I_Rx_Amp/np.std(I_Rx_Amp)
 
 # capture samples in the middle of signaling intervals
@@ -334,5 +335,3 @@ plt.legend()
 plt.grid()
 plt.ylim(0, 1.5)
 plt.xlim(0,err.size)
-
-
