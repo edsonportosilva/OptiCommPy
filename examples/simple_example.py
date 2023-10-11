fa2daf59e7ec90698987fac2585707605835a57d
Edson Porto da Silva <edsonporto88@gmail.com>
2023-10-11 11:13:42 -0300

    Update simple_example.py

--- examples/simple_example.py
+++ examples/simple_example.py
@@ -1,20 +1,14 @@
 
 import numpy as np
 from commpy.utilities  import upsample
-from optic.models.devices import mzm, photodiode, edfa
+from optic.models.devices import mzm, photodiode
 from optic.models.channels import linFiberCh
 from optic.comm.modulation import modulateGray
 from optic.dsp.core import firFilter, pulseShape, pnorm, signal_power
 from optic.core import parameters
-from optic.plot import eyediagram
-import matplotlib.pyplot as plt
 from scipy.special import erfc
-from tqdm.notebook import tqdm
-import scipy as sp
 
-
-# ### Intensity modulation (IM) with On-Off Keying (OOK)
-
+## Intensity modulation (IM) with On-Off Keying (OOK)
 
 # simulation parameters
 SpS = 16    # samples per symbol
@@ -28,6 +22,14 @@ Vb = -Vπ/2
 Pi_dBm = 0 # laser optical power at the input of the MZM in dBm
 Pi = 10**(Pi_dBm/10)*1e-3 # convert from dBm to W
 
+# linear fiber optical channel parameters
+L = 90         # total link distance [km]
+α = 0.2        # fiber loss parameter [dB/km]
+D = 16         # fiber dispersion parameter [ps/nm/km]
+Fc = 193.1e12  # central optical frequency [Hz]
+
+## Simulation
+print('\nStarting simulation...')
 # generate pseudo-random bit sequence
 bitsTx = np.random.randint(2, size=100000)
 
@@ -49,65 +51,12 @@ sigTx = firFilter(pulse, symbolsUp)
 Ai = np.sqrt(Pi)*np.ones(sigTx.size)
 sigTxo = mzm(Ai, sigTx, Vπ, Vb)
 
-print('Average power of the modulated optical signal [mW]: %.3f mW'%(signal_power(sigTxo)/1e-3))
-print('Average power of the modulated optical signal [dBm]: %.3f dBm'%(10*np.log10(signal_power(sigTxo)/1e-3)))
+print('\nAverage power of the transmitted optical signal [dBm]: %.3f dBm'%(10*np.log10(signal_power(sigTxo)/1e-3)))
 
-fig, axs = plt.subplots(1, 2, figsize=(16,3))
-interval = np.arange(16*20,16*50)
-t = interval*Ts/1e-9
-
-# plot psd
-axs[0].set_xlim(-3*Rs,3*Rs);
-axs[0].set_ylim(-180,-80);
-axs[0].psd(sigTx,Fs=Fs, NFFT = 16*1024, sides='twosided', label = 'RF signal spectrum')
-axs[0].legend(loc='upper left');
-
-axs[1].plot(t, sigTx[interval], label = 'RF binary signal', linewidth=2)
-axs[1].set_ylabel('Amplitude (a.u.)')
-axs[1].set_xlabel('Time (ns)')
-axs[1].set_xlim(min(t),max(t))
-axs[1].legend(loc='upper left')
-axs[1].grid()
-
-fig, axs = plt.subplots(1, 2, figsize=(16,3))
-# plot psd
-axs[0].set_xlim(-3*Rs,3*Rs);
-axs[0].set_ylim(-230,-130);
-axs[0].psd(np.abs(sigTxo)**2, Fs=Fs, NFFT = 16*1024, sides='twosided', label = 'Optical signal spectrum')
-axs[0].legend(loc='upper left');
-
-axs[1].plot(t, np.abs(sigTxo[interval])**2, label = 'Optical modulated signal', linewidth=2)
-axs[1].set_ylabel('Power (p.u.)')
-axs[1].set_xlabel('Time (ns)')
-axs[1].set_xlim(min(t),max(t))
-axs[1].legend(loc='upper left')
-axs[1].grid()
-# -
-
-# ### Linear fiber channel model (fiber + EDFA opt. amplifier)
-
-# +
-# linear optical channel
-L = 50         # total link distance [km]
-α = 0.2        # fiber loss parameter [dB/km]
-D = 16         # fiber dispersion parameter [ps/nm/km]
-Fc = 193.1e12  # central optical frequency [Hz]
-
+# Linear fiber channel model 
 sigCh = linFiberCh(sigTxo, L, α, D, Fc, Fs)
 
-# receiver pre-amplifier
-G = α*L    # edfa gain
-NF = 4.5   # edfa noise figure
-sigCh = edfa(sigCh, Fs, G, NF, Fc)
-# -
-
-# ### Direct-detection (DD) pin receiver model
-
-# +
-# ideal photodiode (noiseless, no bandwidth limitation)
-paramPD = parameters()
-paramPD.ideal = True
-I_Rx_ideal = photodiode(sigTxo.real, paramPD)
+# Direct-detection (DD) pin receiver model
 
 # noisy photodiode (thermal noise + shot noise + bandwidth limitation)
 paramPD = parameters()
@@ -116,13 +65,6 @@ paramPD.B = Rs
 paramPD.Fs = Fs
 
 I_Rx = photodiode(sigCh, paramPD)
-
-discard = 100
-
-eyediagram(I_Rx_ideal[discard:-discard], I_Rx.size-2*discard, SpS, plotlabel='signal at Tx', ptype='fancy')
-eyediagram(I_Rx[discard:-discard], I_Rx.size-2*discard, SpS, plotlabel='signal at Rx', ptype='fancy')
-
-# +
 I_Rx = I_Rx/np.std(I_Rx)
 
 # capture samples in the middle of signaling intervals
@@ -138,6 +80,17 @@ I0 = np.mean(I_Rx[bitsTx==0]) # average value of I0
 Id = (σ1*I0 + σ0*I1)/(σ1 + σ0) # optimal decision threshold
 Q = (I1-I0)/(σ1 + σ0) # factor Q
 
+# apply the optimal decision rule
+bitsRx = np.empty(bitsTx.size)
+bitsRx[I_Rx> Id] = 1
+bitsRx[I_Rx<= Id] = 0
+
+# calculate the BER
+discard = 0
+err = np.logical_xor(bitsRx[discard:bitsRx.size-discard], bitsTx[discard:bitsTx.size-discard])
+BER = np.mean(err) 
+
+print('\nReceived signal parameters:')
 print('I0 = %.2f '%(I0))
 print('I1 = %.2f '%(I1))
 print('σ0 = %.2f '%(σ0))
@@ -145,28 +98,8 @@ print('σ1 = %.2f '%(σ1))
 print('Optimal decision threshold Id = %.2f '%(Id))
 print('Q = %.2f \n'%(Q))
 
-# Apply the optimal decision rule
-bitsRx = np.empty(bitsTx.size)
-bitsRx[I_Rx> Id] = 1
-bitsRx[I_Rx<= Id] = 0
-
-discard = 100
-err = np.logical_xor(bitsRx[discard:bitsRx.size-discard], bitsTx[discard:bitsTx.size-discard])
-BER = np.mean(err)
-
 Pb = 0.5*erfc(Q/np.sqrt(2)) # theoretical error probability
 print('Number of counted errors = %d '%(err.sum()))
 print('BER = %.2e '%(BER))
 print('Pb = %.2e '%(Pb))
-
-err = err*1.0
-err[err==0] = np.nan
-
-plt.plot(err,'o', label = 'bit errors')
-plt.vlines(np.where(err>0), 0, 1)
-plt.xlabel('bit position')
-plt.ylabel('counted error')
-plt.legend()
-plt.grid()
-plt.ylim(0, 1.5)
-plt.xlim(0,err.size);
+print('\nSimulation ended')
