8a9ab72895b4d384cafa829c651cf03047980e60
Edson Porto da Silva <edsonporto88@gmail.com>
2023-10-13 00:04:12 -0300

    Update simple_example.py

--- examples/simple_example.py
+++ examples/simple_example.py
@@ -2,9 +2,9 @@
 import numpy as np
 from commpy.utilities  import upsample
 from optic.models.devices import mzm, photodiode
-from optic.models.channels import linFiberCh
+from optic.models.channels import linearFiberChannel
 from optic.comm.modulation import modulateGray
-from optic.dsp.core import firFilter, pulseShape, signal_power
+from optic.dsp.core import firFilter, pulseShape
 from optic.core import parameters
 from scipy.special import erfc
 
@@ -17,6 +17,10 @@ Rs = 10e9   # Symbol rate
 Fs = SpS*Rs # Signal sampling frequency (samples/second)
 np.random.seed(seed=123) # fixing the seed to get reproducible results
 
+# typical NRZ pulse
+pulse = pulseShape('nrz', SpS)
+pulse = pulse/max(abs(pulse)) # normalize to 1 Vpp
+
 # MZM parameters
 Vpi = 2
 Vb = -Vpi/2
@@ -24,10 +28,12 @@ Pi_dBm = 0 # laser optical power at the input of the MZM in dBm
 Pi = 10**(Pi_dBm/10)*1e-3 # convert from dBm to W
 
 # linear fiber optical channel parameters
-L = 90         # total link distance [km]
-alpha = 0.2        # fiber loss parameter [dB/km]
-D = 16         # fiber dispersion parameter [ps/nm/km]
-Fc = 193.1e12  # central optical frequency [Hz]
+paramCh = parameters()
+paramCh.L = 90         # total link distance [km]
+paramCh.alpha = 0.2    # fiber loss parameter [dB/km]
+paramCh.D = 16         # fiber dispersion parameter [ps/nm/km]
+paramCh.Fc = 193.1e12  # central optical frequency [Hz]
+paramCh.Fs = Fs
 
 # photodiode parameters
 paramPD = parameters()
@@ -47,10 +53,6 @@ symbTx = modulateGray(bitsTx, M, 'pam')
 # upsampling
 symbolsUp = upsample(symbTx, SpS)
 
-# typical NRZ pulse
-pulse = pulseShape('nrz', SpS)
-pulse = pulse/max(abs(pulse)) # normalize to 1 Vpp
-
 # pulse shaping
 sigTx = firFilter(pulse, symbolsUp)
 
@@ -58,10 +60,8 @@ sigTx = firFilter(pulse, symbolsUp)
 Ai = np.sqrt(Pi)*np.ones(sigTx.size) # ideal cw laser envelope
 sigTxo = mzm(Ai, sigTx, Vpi, Vb)
 
-# Linear fiber channel model 
-sigCh = linFiberCh(sigTxo, L, alpha, D, Fc, Fs)
-
-# Direct-detection (DD) pin receiver model
+# linear fiber channel model
+sigCh = linearFiberChannel(sigTxo, paramCh)
 
 # noisy photodiode (thermal noise + shot noise + bandwidth limitation)
 I_Rx = photodiode(sigCh, paramPD)
@@ -74,11 +74,11 @@ I_Rx = I_Rx[0::SpS]
 I1 = np.mean(I_Rx[bitsTx==1]) # average value of I1
 I0 = np.mean(I_Rx[bitsTx==0]) # average value of I0
 
-σ1 = np.std(I_Rx[bitsTx==1]) # standard deviation σ1 of I1
-σ0 = np.std(I_Rx[bitsTx==0]) # standard deviation σ0 of I0
+std1 = np.std(I_Rx[bitsTx==1]) # standard deviation std1 of I1
+std0 = np.std(I_Rx[bitsTx==0]) # standard deviation std0 of I0
 
-Id = (σ1*I0 + σ0*I1)/(σ1 + σ0) # optimal decision threshold
-Q = (I1-I0)/(σ1 + σ0) # factor Q
+Id = (std1*I0 + std0*I1)/(std1 + std0) # optimal decision threshold
+Q = (I1-I0)/(std1 + std0) # Q factor 
 
 # apply the optimal decision rule
 bitsRx = np.empty(bitsTx.size)
@@ -92,8 +92,8 @@ BER = np.mean(err)
 print('\nReceived signal parameters:')
 print(f'I0 = {I0:.2f}')
 print(f'I1 = {I1:.2f}')
-print(f'σ0 = {σ0:.2f}')
-print(f'σ1 = {σ1:.2f}')
+print(f'std0 = {std0:.2f}')
+print(f'std1 = {std1:.2f}')
 print(f'Optimal decision threshold Id = {Id:.2f}')
 print(f'Q = {Q:.2f} \n')
 
