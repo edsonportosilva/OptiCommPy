f508a493ee253a05ca88d0f418c3ae73ea0524dc
Edson Porto da Silva <edsonporto88@gmail.com>
2023-11-26 11:47:04 -0300

    Create test_metrics.py

---
+++ tests/test_metrics.py
@@ -0,0 +1,108 @@
+import unittest
+import numpy as np
+from optic.dsp.core import pnorm
+from optic.models.channels import awgn
+from optic.comm.modulation import modulateGray
+from optic.comm.metrics import (
+    fastBERcalc,
+    calcLLR,
+    monteCarloGMI,
+    monteCarloMI,
+    Qfunc,
+    calcEVM,
+    theoryBER,
+    calcLinOSNR,
+    GN_Model_NyquistWDM,
+    ASE_NyquistWDM,
+    GNmodel_OSNR,
+)
+
+class TestCommunicationMetrics(unittest.TestCase):
+    
+    def setUp(self):
+        # Set up common parameters or fixtures if needed
+        pass
+
+    def test_fastBERcalc(self):
+        # Add test cases for fastBERcalc
+
+        # Run BER vs Ebn0 Monte Carlo simulation in the AWGN channel
+
+        qamOrder  = [4, 16, 64, 256]  # Modulation order
+
+        EbN0dB_  = np.arange(10, 25.5, 0.5)
+        BER      = np.zeros((len(EbN0dB_),len(qamOrder)))
+        BER_theory = np.zeros((len(EbN0dB_),len(qamOrder)))
+
+        for ii, M in enumerate(qamOrder):
+        
+            for indSNR in range(EbN0dB_.size):
+
+                EbN0dB = EbN0dB_[indSNR]
+
+                # generate random bits
+                bitsTx = np.random.randint(2, size=int(np.log2(M)*1e5))    
+
+                # Map bits to constellation symbols
+                symbTx = modulateGray(bitsTx, M, 'qam')
+
+                # Normalize symbols energy to 1
+                symbTx = pnorm(symbTx) 
+
+                # AWGN channel  
+                snrdB  = EbN0dB + 10*np.log10(np.log2(M))
+                symbRx = awgn(symbTx, snrdB)
+
+                # BER calculation
+                BER[indSNR, ii], _, _ = fastBERcalc(symbRx, symbTx, M, 'qam')
+                BER_theory[indSNR, ii] = theoryBER(M, EbN0dB, 'qam')
+
+                if BER[indSNR, ii] == 0:              
+                    break
+
+        np.testing.assert_array_almost_equal(BER, BER_theory, decimal=3)
+
+        
+
+    def test_calcLLR(self):
+        # Add test cases for calcLLR
+        pass
+
+    def test_monteCarloGMI(self):
+        # Add test cases for monteCarloGMI
+        pass
+
+    def test_monteCarloMI(self):
+        # Add test cases for monteCarloMI
+        pass
+
+    def test_Qfunc(self):
+        # Add test cases for Qfunc
+        pass
+
+    def test_calcEVM(self):
+        # Add test cases for calcEVM
+        pass
+
+    def test_theoryBER(self):
+        # Add test cases for theoryBER
+        pass
+
+    def test_calcLinOSNR(self):
+        # Add test cases for calcLinOSNR
+        pass
+
+    def test_GN_Model_NyquistWDM(self):
+        # Add test cases for GN_Model_NyquistWDM
+        pass
+
+    def test_ASE_NyquistWDM(self):
+        # Add test cases for ASE_NyquistWDM
+        pass
+
+    def test_GNmodel_OSNR(self):
+        # Add test cases for GNmodel_OSNR
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
