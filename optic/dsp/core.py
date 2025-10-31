9a1416f1a402421d34f562bcb280858006074fb1
Edson Porto da Silva <17885240+edsonportosilva@users.noreply.github.com>
2025-10-31 16:00:22 -0300

    Remove syncDataSequences function from core.py
    The syncDataSequences function and its reference in the module docstring have been removed from optic.dsp.core.

--- optic/dsp/core.py
+++ optic/dsp/core.py
@@ -20,7 +20,6 @@ Core digital signal processing utilities (:mod:`optic.dsp.core`)
    upsample               -- Upsample a signal by inserting zeros between samples.
    symbolSync             -- Synchronizer delayed sequences of symbols.
    finddelay              -- Estimate the delay between sequences of symbols.
-   syncDataSequences      -- Synchronize data sequences with a given reference.
    pnorm                  -- Normalize the average power of each componennt of x.
    gaussianComplexNoise   -- Generate complex-valued circular Gaussian noise.
    gaussianNoise          -- Generate Gaussian noise.
@@ -307,8 +306,9 @@ def clockSamplingInterp(x, Fs_in, Fs_out, jitter_rms=0):
     tin = np.arange(0, x.shape[0]) * inTs
     tout = np.arange(0, x.shape[0] * inTs, outTs)
 
-    jitter = np.random.normal(0, jitter_rms, tout.shape)
-    tout += jitter
+    if jitter_rms > 0:
+        jitter = np.random.normal(0, jitter_rms, tout.shape)
+        tout += jitter
 
     y = np.zeros((len(tout), x.shape[1]), dtype=x.dtype)
 
@@ -657,113 +657,6 @@ def finddelay(x, y):
 
     """
     return np.argmax(np.abs(signal.correlate(x, y))) - x.shape[0] + 1
-
-
-def syncDataSequences(rx, tx, param):
-    """
-    Synchronize data sequences with a given reference.
-
-    Parameters
-    ----------
-    rx : np.array
-        Received signal.
-    tx : np.array
-        Transmitted reference signal.
-    param : optic.utils.parameters object, optional
-        Parameters of the synchronization process.
-
-        - param.SpS : samples per symbol of the transmitted signal.
-        - param.reference : string ('signal','symbols')
-        - param.pulseType : string ('rect','nrz','rrc','rc', 'doubinary')
-        - param.rollOff : rolloff of RRC filter. Default is 0.1.
-        - param.nFilterTaps : number of filter coefficients. Default is 1024.
-
-
-    Returns
-    -------
-    np.array
-        Synchronized received signal.
-
-    Notes
-    -----
-    Signals x and y must have the same number of columns (modes).
-
-    """
-    SpS = getattr(param, "SpS", 1)
-    reference = getattr(param, "reference", "signal")
-    pulseType = getattr(param, "pulseType", "rc")
-    rollOff = getattr(param, "rollOff", 0.01)
-    nFilterTaps = getattr(param, "nFilterTaps", 1024)
-
-    # generate pulse shaping filter
-    paramPS = parameters()
-    paramPS.pulseType = pulseType
-    paramPS.SpS = SpS
-    paramPS.rollOff = rollOff
-    paramPS.nFilterTaps = nFilterTaps
-    pulse = pulseShape(paramPS)
-
-    try:
-        rx.shape[1]
-        input1D = False
-    except IndexError:
-        input1D = True
-        # If rx is a 1D array, reshape it to a 2D array
-        rx = rx.reshape(len(rx), 1)
-
-    try:
-        tx.shape[1]
-    except IndexError:
-        # If tx is a 1D array, reshape it to a 2D array
-        tx = tx.reshape(len(tx), 1)
-
-    if reference == "symbols":
-        # Upsample transmitted signal
-        tx = upsample(tx, SpS)
-
-    # find repetitions of the transmitted signal to match length of received signal
-    repeats = np.ceil(rx.shape[0] / tx.shape[0])
-    tx_ = np.tile(tx, (int(repeats), 1))
-
-    # calculate required padding
-    padL = tx_.shape[0] - rx.shape[0]
-
-    if padL > 0:
-        # pad received signal
-        rx = np.pad(rx, ((0, padL), (0, 0)))
-
-    # synchronize signals
-    tx_ = symbolSync(rx, tx_, 1)
-    tx_ = tx_[0 : rx.shape[0] - padL, :]
-
-    if reference == "symbols":
-        symb = pnorm(tx_[tx_ != 0])  # extract transmitted symbols
-        # generate waveform from synchronized symbols
-        tx_ = firFilter(pulse, tx_)
-        tx_ = pnorm(tx_)
-    elif reference == "signal":
-        paramRes = parameters()
-        paramRes.inFs = SpS
-        paramRes.outFs = 25
-        x = resample(tx_, paramRes)
-
-        # paramPS.SpS = paramRes.outFs
-        # paramPS.rollOff = rollOff
-        # paramPS.nFilterTaps = int(paramRes.outFs / paramRes.inFs) * nFilterTaps
-        # pulse = pulseShape(paramPS)
-        # x = firFilter(pulse, x)
-
-        paramDec = parameters()
-        paramDec.SpSin = paramRes.outFs
-        paramDec.SpSout = 1
-        nSymb = np.floor(x.shape[0] // paramDec.SpSin)
-        symb = decimate(x[0 : int(nSymb * paramDec.SpSin), :], paramDec)
-        symb = pnorm(symb)
-
-    if input1D:
-        tx_ = tx_.flatten()
-
-    return tx_, symb
 
 
 @njit
